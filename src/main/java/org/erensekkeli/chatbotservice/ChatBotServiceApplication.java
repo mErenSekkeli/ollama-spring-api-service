dab0076962058e2bbd0a1c38f4895e691ad8d28b
erensekkeli <erensekkeli46@gmail.com>
2024-05-11 16:53:42 +0300

    initial commit

---
+++ src/main/java/org/erensekkeli/chatbotservice/ChatBotServiceApplication.java
@@ -0,0 +1,13 @@
+package org.erensekkeli.chatbotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/erensekkeli/chatbotservice/ChatBotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erensekkeli.chatbotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
